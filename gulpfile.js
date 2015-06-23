adc8e37e9990d75c3b21e4266a1c93e9ca9709ab
Michael Miriti <michael@miriti.ru>
2015-06-23 13:11:28 +0300

    ignore node_modules. 80 port for webserver

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 
 gulp.task('webserver', function() {
-    gulp.src('static').pipe(webserver());
+    gulp.src('static').pipe(webserver({
+        port: 80
+    }));
 });
 
 gulp.task('default', ['webserver']);
